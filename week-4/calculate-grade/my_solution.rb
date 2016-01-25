adc4ac94329fd08a940c0e8bf0749b23d3482b23
John Pults <jill.kills.jack@gmail.com>
2016-01-25 13:00:11 +0000

    4.5

--- week-4/calculate-grade/my_solution.rb
+++ week-4/calculate-grade/my_solution.rb
@@ -4,3 +4,21 @@
 
 
 # Your Solution Below
+
+def get_grade average
+  if average <= 100 && average > 89
+    return 'A'
+  end
+  if average <= 89 && average > 79
+    return 'B'
+  end
+  if average <= 79 && average > 69
+    return 'C'
+  end
+  if average <= 69 && average > 59
+    return 'D'
+  end
+  if average <= 59
+    return 'F'
+  end
+end
