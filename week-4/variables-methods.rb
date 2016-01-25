a0f9aa61fbf3d802f9841adacfc18b5c59469c3e
John Pults <jill.kills.jack@gmail.com>
2016-01-25 15:03:23 +0000

    Methods

---
+++ week-4/variables-methods.rb
@@ -0,0 +1,25 @@
+
+puts "Enter your first name:"
+first = gets.chomp
+puts "Enter your middle name:"
+middle = gets.chomp
+puts "Enter your last name:"
+last = gets.chomp
+
+puts "Hello #{first} #{middle} #{last}"
+
+
+puts "What's your favorite number?"
+number = gets.to_i
+number += 1
+puts "#{number} is bigger and better though."
+
+
+# begin
+# How do you define a local variable?  The same way you define a regular variable, but inside a method.
+# How do you define a method?   def method_name (method_argument, method_argument)
+# What is the difference between a local variable and a method? A method is a program within a program that can be called to do something. A local variable is a variable within the method.
+# How do you run a ruby program from the command line?  ruby ruby_filename.rb
+# How do you run an RSpec file from the command line?  rspec rspec_file.rb
+# What was confusing about this material? What made sense?  It was fairly straightforward for me. I don't know about rspec though or how it works.
+# end
