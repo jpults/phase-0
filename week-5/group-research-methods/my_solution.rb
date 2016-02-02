af24e90a2d4fbaf6d69fa77a89a09face2c1b213
John Pults <jill.kills.jack@gmail.com>
2016-02-02 11:54:05 +0000

    Group Stuff

--- week-5/group-research-methods/my_solution.rb
+++ week-5/group-research-methods/my_solution.rb
@@ -5,9 +5,57 @@
 i_want_pets = ["I", "want", 3, "pets", "but", "only", "have", 2]
 my_family_pets_ages = {"Evi" => 6, "Ditto" => 3, "Hoobie" => 3, "George" => 12, "Bogart" => 4, "Poly" => 4, "Annabelle" => 0}
 
+# Person 3
+def my_array_sorting_method(source)
+    source.sort_by {|x| x.to_s}
+end
 
+def my_hash_sorting_method(source)
+  source.sort_by {|k,v| v}
+end
+my_hash_sorting_method(my_family_pets_ages)
+#1 Identify and describe the Ruby method(s) you implemented.
+# I used the .sort_by method. For the first problem, I was expected to sort the array in alphabetical order. 
+#Knowing that the array had integers, I knew I was going to have to modify the integers to become strings. 
+#Therefore, I used the .sort_by method, and I told my computer to sort by x,and when going through each item in the 
+#array convert them to strings. This created the desired output. For the second problem, I used the same method, the 
+#difference is I wanted to sort by the value, so I clearly told my computer there are 2 variables, but I am only requesting
+# the variable V. This created the desired output.
+#2Teach your accountability group how to use the methods
+#As you can see in my code I defined my method that accepts source as an argumement. Then I used .sort_by to sort each of the 
+#arguments into a string. This is a pretty simple method and code. 
+#3Share any tricks you used to find and decipher the Ruby Docs
+#I didn't learn any particular tricks for this particular method. I feel like using built in methods feel like a trick already!
 # #
 # #
+def my_array_deletion_method!(array, x)
+
+  array.delete_if{|str| str.to_s.include?(x)}
+
+end
+
+my_array_deletion_method!(i_want_pets, "a")
+
+p i_want_pets
+
+
+def my_hash_deletion_method!(hash, name)
+
+hash.delete_if{|key| key.include?(name)}
+
+end
+
+my_hash_deletion_method!(my_family_pets_ages, "George")
+
+p my_family_pets_ages
+
+=begin
+Identify and describe the Ruby method(s) you implemented.
+The Ruby methods I implemented were .delete_if, .include?, and .to_s.
+> .delete_if - Deletes every key-value pair from array or hash for which block evaluates to true.
+>.include? - Returns true if the given object is present in self (that is, if any element == object), otherwise returns false.
+>.to_s - Creates a string representation of self.
+=end
 
 
 # Person 5
