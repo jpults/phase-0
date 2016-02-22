bacf4ee8e4362203aa38bc9e8a28c99b144b7982
jpults <jill.kills.jack@gmail.com>
2016-02-22 07:39:24 -0600

    Update ruby_review.rb

--- week-8/ruby_review.rb
+++ week-8/ruby_review.rb
@@ -31,9 +31,9 @@ end
   return array
 end
 
+//This is a program to print the nth row of Pascal's Triangle
 
 
-
-What concepts did you review or learn in this challenge?  Arrays, for loops, each loops, and methods.
-What is still confusing to you about Ruby?  The different datatypes and how they are accessed. 
-What are you going to study to get more prepared for Phase 1? Hashes, regular expressions.
+//What concepts did you review or learn in this challenge?  Arrays, for loops, each loops, and methods.
+//What is still confusing to you about Ruby?  The different datatypes and how they are accessed. 
+//What are you going to study to get more prepared for Phase 1? Hashes, regular expressions.
